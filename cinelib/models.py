d8fad59538c6669d2fb63824c5ad5d7c7e8f7b01
RuanSampaio-code <ruansampaio001@gmail.com>
2025-02-23 21:11:25 -0300

    ajuste no banco de dados

--- cinelib/models.py
+++ cinelib/models.py
@@ -1,29 +1,21 @@
 from cinelib import database
+from cinelib import database
 
 class Usuario(database.Model):
-    id = database.Column(database.Integer, primary_key = True)
+    id = database.Column(database.Integer, primary_key=True)
     username = database.Column(database.String, nullable=False)
     email = database.Column(database.String, nullable=False, unique=True)
     senha = database.Column(database.String, nullable=False)
-    filme = database.relationship("Filmes", backref="usuario", lazy=True)
-    serie = database.relationship("Series", backref="usuario", lazy=True)
+    midias = database.relationship("Midia", backref="usuario", lazy=True)  # Relacionamento 1:N
 
-class Filmes(database.Model):
-    id = database.Column(database.Integer, primary_key = True)
-    titulo_filme = database.Column(database.String, nullable=False)
-    sinopse = database.Column(database.String, nullable=False)
-    ano = database.Column(database.DateTime, nullable=False)
-    genero = database.Column(database.String, nullable=False)
-    duracao = database.Column(database.Integer, nullable=False)
-    foto = database.Column(database.String)
-    id_usuario = database.Column(database.Integer, database.ForeignKey("usuario.id") ,nullable=False)
-
-class Series(database.Model):
-    id = database.Column(database.Integer, primary_key = True)
+class Catalogo(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
     titulo = database.Column(database.String, nullable=False)
     sinopse = database.Column(database.String, nullable=False)
-    ano = database.Column(database.DateTime, nullable=False)
+    ano = database.Column(database.Integer, nullable=False)  # Ano de lançamento
     genero = database.Column(database.String, nullable=False)
-    duracao = database.Column(database.Integer, nullable=False)
+    duracao = database.Column(database.Integer, nullable=False)  # Minutos para filmes / episódios para séries
     foto = database.Column(database.String)
-    id_usuario = database.Column(database.Integer, database.ForeignKey("usuario.id") ,nullable=False)
+    tipo = database.Column(database.String, nullable=False)  # Pode ser "Filme" ou "Série"
+    assistido = database.Column(database.Boolean, default=False, nullable=False)  # Indica se já foi assistido
+    id_usuario = database.Column(database.Integer, database.ForeignKey("usuario.id"), nullable=False)  # Chave estrangeira
--- criar_banco.py
+++ criar_banco.py
@@ -1,5 +1,5 @@
 from cinelib import database, app
-from cinelib.models import Usuario, Filmes, Series
+from cinelib.models import Usuario, Catgialogo
 
 with app.app_context():
     database.create_all()
